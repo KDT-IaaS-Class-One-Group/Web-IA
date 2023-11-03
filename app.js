85a209dc665bdd7587f17119fcb0782a6f11b0a5
ohseungmin <ohmin2886@gmail.com>
2023-11-03 12:43:25 +0900

    express GET 요청 작성 파일 변경

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const express = require('express');
+const path = require('path');
+const { listenerCount } = require('process');
+const port = 4040;
+const app = express();
+
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'frontEnd/index.html');
+  res.sendFile(indexPath);
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
