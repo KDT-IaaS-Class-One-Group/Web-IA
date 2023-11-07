10971c66694b7df71200b3e4125fd08c20865040
ohseungmin <ohmin2886@gmail.com>
2023-11-07 17:56:28 +0900

    파일 생성 코드에서 파일명을 다시 지정 GPT의 파일 생성 방식이 맘에 안듬

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const router = express.Router();
 const fs = require('fs');
 
 app.use(bodyParser.json());
+// 정적 미들웨어 작성
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get으로 / 맨 처음 경로를 설정해야 서버 생성이 가능
+// 그렇지 않을 경우 cannot GET / 문구 발생
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
@@ -19,16 +22,27 @@ app.get('/subData', (req, res) => {
 });
 
 // POST 요청 처리
+// app.post('', () => ) 앞쪽 경로가 매우 중요하다.
+// 
 app.post('/sub', (req, res) => {
   // 선언명 body에 요청
   const requestData = req.body;
   console.log('POST 요청 데이터:', requestData);
 
   // JSON 파일명을 현재 시간을 기반으로 생성 (예: 2023-11-07-12-34-56.json)
-  const timestamp = new Date().toISOString().replace(/[-:]/g, '');
-  const jsonFileName = `${timestamp}.json`;
+//  const timestamp = new Date().toISOString().replace(/[-:]/g, '');
+let counter = 1;
+    function fileName() {
+      const DataName = `index${counter}.json`
+      counter++;
+      return DataName;
+    }
+
+    const jsonFileName = fileName();
+    
 
   // JSON 파일을 생성하고 데이터를 저장
+  // JSON.stringify()메서드 JavaScript 값이나 객체를 JSON 문자열로 변환
   fs.writeFile(jsonFileName, JSON.stringify(requestData, null, 2), err => {
     if (err) {
       console.error(err);
