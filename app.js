dbfbeb14a18c3c374069ca89ed2c7572c7fd742c
ohseungmin <ohmin2886@gmail.com>
2023-11-07 14:59:45 +0900

    다시 작성 시작

--- app.js
+++ app.js
@@ -4,29 +4,24 @@ const bodyParser = require('body-parser');
 const port = 4040;
 const app = express();
 const router = express.Router();
-
-// index.html 파일이 있는 디렉토리 경로
-const staticFilePath = path.join(__dirname, 'frontEnd');
-
-// 정적 파일을 서빙하는 미들웨어를 추가
-app.use(express.static(staticFilePath));
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(staticFilePath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontEnd/index.html'));
 });
 
-app.use(bodyParser.json());
-let reData = {value: ''};
-
-app.post('/sub', (req, res) => {
-  reData = { value : req.body.value };
-  const responseData = {message: 'POST 요청 성공적'}
-  res.json(responseData)
+app.get('/data', (req, res) => {
+  // 데이터 조회 로직을 작성
+  res.json({ message: 'GET 요청에 대한 응답' });
 });
 
-app.get('/subData', (req, res) => {
-  res.json(reData);
-})
+// POST 요청 처리
+app.post('/data', (req, res) => {
+  // POST 요청으로 받은 데이터 처리
+  const requestData = req.body;
+  // 여기에서 데이터를 저장하거나 다룰 수 있음
+  res.json({ message: 'POST 요청에 대한 응답', data: requestData });
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
