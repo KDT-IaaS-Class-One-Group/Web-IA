dda1c4d69a1dc633599501d414f22b9e8f0744e0
ohseungmin <ohmin2886@gmail.com>
2023-11-07 16:13:20 +0900

    정적 미들웨어로 CSS 파일을 호출 POST요청으로 json 데이터 저장 확인

--- app.js
+++ app.js
@@ -6,21 +6,28 @@ const app = express();
 const router = express.Router();
 const fs = require('fs');
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontEnd/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/data', (req, res) => {
+app.get('/subData', (req, res) => {
   // 데이터 조회 로직을 작성
   res.json({ message: 'GET 요청에 대한 응답' });
 });
 
 // POST 요청 처리
-app.post('/data', (req, res) => {
-  // POST 요청으로 받은 데이터 처리
+app.post('/sub', (req, res) => {
   const requestData = req.body;
-  // 여기에서 데이터를 저장하거나 다룰 수 있음
-  res.json({ message: 'POST 요청에 대한 응답', data: requestData });
+  // requestData 객체에는 클라이언트가 POST 요청으로 보낸 JSON 데이터가 들어 있습니다
+  console.log('POST 요청 데이터:', requestData);
+
+  // 여기에서 requestData를 원하는 방식으로 처리할 수 있습니다.
+
+  // JSON 형식의 응답을 반환합니다
+  res.json({ message: 'POST 요청에 대한 응답', receivedData: requestData });
 });
 
 app.listen(port, () => {
