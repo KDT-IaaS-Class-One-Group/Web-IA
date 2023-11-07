3e67f3ae9b24a1fd48d6795cdcfe4d1426eabab9
ohseungmin <ohmin2886@gmail.com>
2023-11-07 11:04:47 +0900

    404 에러 확인

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { listenerCount } = require('process');
+const bodyParser = require('body-parser')
 const port = 4040;
 const app = express();
 
@@ -8,6 +9,23 @@ app.get('/', (req, res) => {
   const indexPath = path.join(__dirname, 'frontEnd/index.html');
   res.sendFile(indexPath);
 });
+
+app.use(bodyParser.json());
+let reData = {value: ''};
+
+app.post('/sub', (req, res) => {
+  reData = { value : req.body.value };
+  const responseData = {message: 'POST 요청 성공적'}
+  res.json(responseData)
+})
+
+app.get('/subData', (req, res) => {
+  const jsonData = {
+    property1 : '값1',
+    property2 : '값2'
+  }
+  res.json(jsonData);
+})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
